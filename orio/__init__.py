331f90a6bdae7a2c082826897dfb6a569bf4100b
Andy Shapiro <shapiromatron@gmail.com>
2016-06-13 15:51:23 -0400

    temporarily undo; may be locking setup.py commands

--- orio/__init__.py
+++ orio/__init__.py
@@ -3,5 +3,5 @@ from .downloadBinaries import binaries_exist, download_ucsc_tools
 __version__ = '0.0.1'
 
 
-if not binaries_exist():
-    download_ucsc_tools()
+# if not binaries_exist():
+#     download_ucsc_tools()
