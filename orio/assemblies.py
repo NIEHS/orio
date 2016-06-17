87d642093aa7144bf6aeb1c05a134eb8075b07fc
Andy Lavender <c.andrew.lavender@gmail.com>
2016-06-17 16:20:24 -0400

    Curate and sort assembly list.  Do not allow 'Patch' entries and sort giving priority to popular model organisms and ENCODE assemblies

--- orio/assemblies.py
+++ orio/assemblies.py
@@ -1,6 +1,7 @@
 import csv
 import pymysql
 import os
+import re
 
 
 def get_UCSC_cursor():
@@ -19,9 +20,26 @@ def get_databases():
 
 
 def get_assemblies():
+    def bring_to_front(_list, value):
+        _list.insert(0, _list.pop(_list.index(value)))
+
     cursor = get_UCSC_cursor()
-    cursor.execute('SELECT table_schema from INFORMATION_SCHEMA.TABLES where table_name="chromInfo";')
-    return [d[0] for d in cursor.fetchall()]
+    cursor.execute('SELECT table_schema from INFORMATION_SCHEMA.TABLES '
+                   'where table_name="chromInfo";')
+    assemblies = [d[0] for d in cursor.fetchall() if 'Patch' not in d[0]]
+
+    for org in reversed(['hg', 'mm', 'dm', 'ce', 'sacCer']):
+        org_list = [assembly for assembly in assemblies
+                    if org == re.split('\d', assembly)[0]]
+        for assembly in sorted(
+            org_list, key=lambda x: int(re.split('\D+', x)[1])
+        ):
+            bring_to_front(assemblies, assembly)
+
+    for assembly in reversed(['hg19', 'mm9']):
+        bring_to_front(assemblies, assembly)
+
+    return assemblies
 
 
 def download_chromosome_sizes(path, overwrite=False):
